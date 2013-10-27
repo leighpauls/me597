2de5ca1bd693ca98a068c7047a6c5c3f940e0bfa
Leigh Pauls <leigh.pauls@gmail.com>
2013-10-27 03:01:49 -0400

    Initial commit, q1q1

---
+++ assignment1/doSimulation.m
@@ -0,0 +1,15 @@
+function [ ] = doSimulation()
+         x = 0;
+         y = 0;
+         heading = 0;
+         hold('off');
+         axis equal;
+         hold('on');
+         for t = [0:0.1:20]
+             if mod(t, 1.0) == 0
+               scatter(x, y, 40, 'r');
+             end
+             [x, y, heading] = bikeModel(x, y, heading, (10 - t) * pi / 180, 0.3);
+         end
+end
+
