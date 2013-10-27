997d3655a9856cc707380bf45490d01f61c4b2fd
Leigh Pauls <leigh.pauls@gmail.com>
2013-10-27 13:55:23 -0400

    Fix indentation

--- assignment1/ackermann.m
+++ assignment1/ackermann.m
@@ -1,9 +1,9 @@
 function [x y heading] = ackermann(dt, length, prevX, prevY, prevHeading, steerAngle, speed)
-%ACKERMANN state-space update of ackermann steering model
-         dist = speed * dt;
-         x = prevX + dist * cos(prevHeading) + normrnd(0, 0.02) * dt;
-         y = prevY + dist * sin(prevHeading) + normrnd(0, 0.02) * dt;
-         dHeading = steerAngle - asin((length - dist) * sin(pi - steerAngle) / length);
-         heading = prevHeading + dt * (dHeading) + normrnd(0, 0.02 * pi / 180) * dt;
+  %ACKERMANN state-space update of ackermann steering model
+  dist = speed * dt;
+  x = prevX + dist * cos(prevHeading) + normrnd(0, 0.02) * dt;
+  y = prevY + dist * sin(prevHeading) + normrnd(0, 0.02) * dt;
+  dHeading = steerAngle - asin((length - dist) * sin(pi - steerAngle) / length);
+  heading = prevHeading + dt * (dHeading) + normrnd(0, 0.02 * pi / 180) * dt;
 end
 
--- assignment1/doSimulation.m
+++ assignment1/doSimulation.m
@@ -1,14 +1,14 @@
 function [ ] = doSimulation()
-         x = 0;
-         y = 0;
-         heading = 0;
-         hold('on');
-         axis equal;
-         for t = [0:0.1:20]
-             if mod(t, 1.0) == 0
-               scatter(x, y, 40, 'r');
-             end
-             [x, y, heading] = bikeModel(x, y, heading, (10 - t) * pi / 180, 3);
-         end
+  x = 0;
+  y = 0;
+  heading = 0;
+  hold('on');
+  axis equal;
+  for t = [0:0.1:20]
+    if mod(t, 1.0) == 0
+      scatter(x, y, 40, 'r');
+    end
+    [x, y, heading] = bikeModel(x, y, heading, (10 - t) * pi / 180, 3);
+  end
 end
 
