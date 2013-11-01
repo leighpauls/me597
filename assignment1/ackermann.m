1ebae7f5c861bde7eebcc54a4a3982ddba565f35
Leigh Pauls <leigh.pauls@gmail.com>
2013-10-31 20:36:11 -0400

    Fix noise on heading

--- assignment1/ackermann.m
+++ assignment1/ackermann.m
@@ -2,7 +2,7 @@ function [x y heading] = ackermann(dt, length, prevX, prevY, prevHeading, steerA
   %ACKERMANN state-space update of ackermann steering model
   dist = speed * dt;
   dHeading = speed * tan(steerAngle) / length;
-  heading = prevHeading + dt * (dHeading) + normrnd(0, 0.02 * pi / 180) * dt;
+  heading = prevHeading + dt * (dHeading) + normrnd(0, 1 * pi / 180) * dt;
   avgHeading = (heading + prevHeading) / 2;
   x = prevX + dist * cos(avgHeading) + normrnd(0, 0.02) * dt;
   y = prevY + dist * sin(avgHeading) + normrnd(0, 0.02) * dt;
