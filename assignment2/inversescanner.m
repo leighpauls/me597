b9d997274459ebdfa6e19bddaa31e5fb47fb5c05
Sarah Elliott <sksellio@gmail.com>
2013-11-26 18:40:47 -0500

    inverse motion model? I hope? untested

---
+++ assignment2/inversescanner.m
@@ -0,0 +1,44 @@
+function [m] = inversescanner(M,N,x,y,theta,meas_phi,meas_r,rmax,alpha,beta)
+% Calculates the inverse measurement model for a laser scanner
+% Identifies three regions, the first where no new information is
+% available, the second where objects are likely to exist and the third
+% where objects are unlikely to exist
+
+% offset of rangers from current position
+% specified clockwise from leftmost sensor
+d = [sqrt(0.10^2 + 0.15^2); sqrt(0.20^2 + 0.15^2); sqrt(0.20^2 + 0.05^2); sqrt(0.10^2 + 0.15^2); sqrt(0.20^2 + 0.15^2); sqrt(0.20^2 + 0.05^2)];    
+
+% 
+
+% Range finder inverse measurement model
+for i = 1:M
+    for j = 1:N
+        % Find range and bearing to the current cell
+        %r = sqrt((i-x)^2+(j-y)^2);
+        phi = mod(atan2(j-y,i-x)-theta+pi,2*pi)-pi;
+        
+        % Find the applicable range measurement 
+        [meas_cur,k] = min(abs(phi-meas_phi));
+        if (meas_cur > beta/2)
+            m(i,j) = 0.5;
+
+        else
+            r = sqrt((i-x)^2+(j-y)^2) - d(k);
+            
+            % If out of range, or behind range measurement, or outside of field
+            % of view, no new information is available
+            if (r > min(rmax, meas_r(k)+alpha/2)) % || (abs(phi-meas_phi(k))>beta/2))
+                m(i,j) = 0.5;
+
+            % If the range measurement was in this cell, likely to be an object
+            elseif ((meas_r(k)< rmax) && (abs(r-meas_r(k))<alpha/2))
+                 m(i,j) = 0.7;
+        
+            % If the cell is in front of the range measurement, likely to be
+            % empty
+            elseif (r < meas_r(k)) 
+                m(i,j) = 0.3;
+            end
+        end
+    end
+end
